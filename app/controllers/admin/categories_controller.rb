b4e85db9ad43294b97f0afb4e1e9ae90d4c729d7
Gord Letkeman <rndedg@gmail.com>
2023-01-05 04:00:06 +0000

    implemented functionality to show list of current categories on admin page along with routes to begin working on creating a new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,30 @@
 class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'].to_s, password: ENV['ADMIN_PASSWORD'].to_s
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:Notice] = 'Category was successfully created'
+      redirect_to [:admin, :categories]
+    else
+      render :new
+    end
+  end
+
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
+
 end
