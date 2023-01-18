980c91bea173ecfd0f76f60426720e0851eff853
Gord Letkeman <rndedg@gmail.com>
2023-01-18 18:06:12 +0000

    Add initial Cypress tests

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,13 @@
+describe("it should visit the home page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("Should show products on the home page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+});
